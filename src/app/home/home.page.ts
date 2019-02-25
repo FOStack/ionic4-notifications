4260f1057d55659a18b014a6ee545b9e848d995f
Felix O <felix.o.stack@gmail.com>
2019-02-24 23:20:51 -0800

    Test Plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(
+  public localNotifications: LocalNotifications,
+  ){
+
+  }
+
+  addNotifications(){
+    this.localNotifications.schedule(
+      {
+        id: 1,
+        title: 'Hello',
+        text: 'This is a test',
+        trigger: {at: new Date(new Date().getTime() + 3600)},
+      }
+    );
+  }
 
 }
